bbd0ebd8033096fd03acf7fb0a4a6ec2c14ce12e
Rubber Goose <rubbergoose246@gmail.com>
2024-07-13 06:20:40 +0000

    2024.07.13 15.20

--- chapter3/3_with_langchain_streamlit/7_langchain-openai.py
+++ chapter3/3_with_langchain_streamlit/7_langchain-openai.py
@@ -1,10 +1,13 @@
 # Pyhton外部モジュールのインポート
+import os
+
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 
 # ChatOpenAIを生成
 chat = ChatOpenAI(
-    openai_api_key="sk-**********",
+    # openai_api_key="sk-**********",
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
     model_name="gpt-3.5-turbo-0125",
 )
 
